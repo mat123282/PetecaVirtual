d03b147a439cd6d821058f94e72239130352ae54
Matheus Salaroli <mat.s@terra.com.br>
2018-12-19 13:10:55 -0200

    Update MapasTreinamento.cs
    Algumas modificações no modo de apresentação dos dados.

--- PetecaVirtual/Assets/Scripts/MapasTreinamento.cs
+++ PetecaVirtual/Assets/Scripts/MapasTreinamento.cs
@@ -7,21 +7,24 @@ public class MapasTreinamento {
     public int numeroMapas;
 
     public  MapasTreinamento() {
-        Mapa mapa1 = new Mapa();
-        mapa1.titulo = "Movimento";
-        mapa1.dificuldade = 1;
-        mapa1.descricao = "Mapa inicial para aprender sobre as movimentações básicas do robo e verificar seu código.";
-        mapa1.Buidindex = 1;
-        mapa1.modoJogo = 1;
         
+        Mapa mapa1 = new Mapa {
+            titulo = "Movimento",
+            dificuldade = 1,
+            descricao = "Mapa inicial para aprender sobre as movimentações básicas do robo e verificar seu código.",
+            Buidindex = 1,
+            modoJogo = 1
+        };
+
         mapas.Add(mapa1);
 
-        Mapa mapa2 = new Mapa();
-        mapa2.titulo = "Curva logo a frente";
-        mapa2.dificuldade = 1;
-        mapa2.descricao = "Teste suas habilidades de rotação com as curvas que você encontrará aqui.";
-        mapa2.Buidindex = 2;
-        mapa2.modoJogo = 1;
+        Mapa mapa2 = new Mapa {
+            titulo = "Curva logo a frente",
+            dificuldade = 1,
+            descricao = "Teste suas habilidades de rotação com as curvas que você encontrará aqui.",
+            Buidindex = 2,
+            modoJogo = 1
+        };
 
         mapas.Add(mapa2);
 
