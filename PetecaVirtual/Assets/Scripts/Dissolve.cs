47405cc5c4108c9e555ef932bb2b927c836bbc69
Matheus Salaroli <mat.s@terra.com.br>
2019-03-10 21:50:50 -0300

    Mudanças no shader
    Criei um novo shader para termos dois.

--- PetecaVirtual/Assets/Scripts/Dissolve.cs
+++ PetecaVirtual/Assets/Scripts/Dissolve.cs
@@ -3,30 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-public class Dissolve : MonoBehaviour
-{
+public class Dissolve : MonoBehaviour {
     private Material mat = null;
     private float height;
     private float progress;
     bool running;
     
-    private void Start()
-    {
+    private void Start() {
         mat = GetComponent<MeshRenderer>().material;
         height = mat.GetFloat("_dissolveSize");
     }
     
-    public void Disolver(int obj,int pts)
-    {
-        
-
-        if (running == false)
-        {
+    public void Disolver(int obj,int pts) {
+        if (running == false) {
             running = true;
             var tracker = FindObjectOfType<ModeTrackingScript>();
             if (!(tracker == null))
-                switch (obj)
-                {
+                switch (obj) {
                     case 1:
                         tracker.pontuacaoRoboVermelho += pts;
                         break;
@@ -36,29 +29,23 @@ public class Dissolve : MonoBehaviour
                 }
 
             mat?.SetFloat("_dissolveSize", height);
-            progress = 1;
+            progress = 0;
             StartCoroutine(Diss());
             Debug.Log($"pontos adicionados ({pts})");
         }
-
-
-
     }
 
-    IEnumerator Diss()
-    {
+    IEnumerator Diss() {
 
         yield return new WaitForFixedUpdate();
-        progress = progress - (0.01f + (Time.deltaTime * (progress)));
+        progress = progress + 2 * (0.01f + (Time.deltaTime * (progress)));
         mat.SetFloat("_progress", progress);
-        if (progress > -0.01)
-        {
+        if (progress < 1) {
             StartCoroutine(Diss());
         }
-        else
-        {
+        else {
             mat?.SetFloat("_dissolveSize", 0);
-            progress = 1;
+            progress = 0;
             yield return null;
             //running = false;
             Destroy(this.gameObject);
